f2047dae5ed788e0233328d32849ef7efafb5bcc
Bijay Limbu <bijaylimbu11111@gmail.com>
2022-04-14 22:21:24 +0545

    Program.cs manage

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseNpgsql(builder
+            .Configuration
+            .GetConnectionString("DefaultConnection")
+            // x=> x.MigrationsAssembly(typeof(TodoDbContext).GetTypeInfo().Assembly.GetName().Name)
+            )
+        .UseSnakeCaseNamingConvention());
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -16,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+app.UseCors(x => x
+
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
